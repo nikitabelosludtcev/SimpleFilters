00cfbbf75bd7f387d129ea2494df4eee83d617f5
Nikita Belosludtcev <nbelosludtcev@gmail.com>
2019-04-28 15:26:17 +0300

    Update project structure

---
+++ AIFactoryPhotoFiltersApp/SupportingFiles/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  AIFactoryPhotoFiltersApp
+//
+//  Created by NIkita on 28/04/2019.
+//  Copyright © 2019 Nikita Belosludtcev. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
